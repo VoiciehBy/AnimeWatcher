4f153f85443006f8cb94c6f4bf96bb471b3d7366
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-01-17 10:37:58 +0100

    Changed BG color...

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require("electron")
 
 const createWindow = () => {
+    
     const window = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        backgroundColor: "#20211f"
     })
 
     window.loadFile("index.html")
